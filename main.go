6f9af9a222acfeb76e0aa4c788d307ae7c62c2dd
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-09-18 10:34:27 -0300

    exibir menu add for

--- main.go
+++ main.go
@@ -10,27 +10,29 @@ func main() {
 	bemVindo()
 	fmt.Println("")
 
-	exibirMenu()
-	comando := lerComando()
-	fmt.Println("")
+	for {
+		exibirMenu()
+		comando := lerComando()
+		fmt.Println("")
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo logs.")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0) //para informar ao computador que saiu com sucesso, sem erros
-	default:
-		fmt.Println("Tente novamente.")
-		os.Exit(-1) //para informar ao computador que algo deu errado
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs.")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0) //para informar ao computador que saiu com sucesso, sem erros
+		default:
+			fmt.Println("Tente novamente.")
+			os.Exit(-1) //para informar ao computador que algo deu errado
+		}
 	}
 }
 
 func bemVindo() {
 	nome := "Kaiqui"
-	versao := 1.3
+	versao := 1.5
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Esse programa está na versão", versao)
 }
@@ -57,6 +59,6 @@ func iniciarMonitoramento() {
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
 	} else {
-		fmt.Println("Site:",site , "esta com problemas. Status Code:", resp.StatusCode)
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
 	}
 }
