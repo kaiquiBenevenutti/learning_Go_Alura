a34e412f0019153b292f455e2ff36a898dd97d75
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-09-18 08:26:03 -0300

    Tipos de variaveis

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Kaiqui"
+	nome2 := "Ju"
+	var idadeKaiqui int = 18
+	idadeJu := 78
+	var numero float32 = 1.5
+	numero2 := 2.55
+
+	fmt.Println("Nome:", nome)
+	fmt.Println("Nome 2:", nome2)
+	fmt.Println("Idade Kaiqui:", idadeKaiqui)
+	fmt.Println("Idade Ju:", idadeJu)
+	fmt.Println("Número 1:", numero)
+	fmt.Println("Número 2:", numero2)
+}
